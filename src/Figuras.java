81d19cd86faa711dfeff448366ee7ec5c5f9f1f3
alu10656117 <aluopr6117@ieselcaminas.org>
2022-11-08 20:16:48 +0100

    ULTIMO COMMIT

--- src/Figuras.java
+++ src/Figuras.java
@@ -1,8 +1,9 @@
 public class Figuras {
     public static void main(String[] args) {
         int x = 2;
-        int y= 5
+        int y= 5;
 
         System.out.println(x+y);
+        //ARCHIVO MODIFICADO PARA SINCRONIZAR CON GITHUB
     }
 }
